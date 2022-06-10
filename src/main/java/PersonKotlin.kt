fd3a841e74a01f1a11a14722e74d814617ecfe28
Kellylin1050 <kellylin1050@gmail.com>
2022-06-10 12:57:13 +0800

    Kotlin hello

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,12 @@
+package com.kelly
+
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin")
+    }
+}
+//1st level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
